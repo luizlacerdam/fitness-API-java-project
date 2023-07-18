9fe0a75d5f51aaa6c0543fa9060078a391c61607
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-18 15:10:16 -0300

    :sparkles: FitnessController-getWorkout

--- src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -1,8 +1,12 @@
 package com.betrybe.fitness.controller;
 
+import com.betrybe.fitness.dto.WorkoutDto;
 import com.betrybe.fitness.service.FitnessService;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +24,14 @@ public class FitnessController implements FitnessControllerInterface {
   public String getRoot() {
     return "Boas vindas à API de Fitness!";
   }
+
+  @GetMapping("/workouts/{id}")
+  public ResponseEntity<WorkoutDto> getWorkout(@PathVariable Long id) {
+    Optional<WorkoutDto> workoutDto = service.getWorkout(id);
+    if (workoutDto.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(workoutDto.get());
+  }
+
 }
